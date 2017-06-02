a5eb6a65130688fb78e27f47f166af82a624999e
signal <signal@kaist.ac.kr>
2017-06-02 23:30:14 +0900

    Modify

--- Extractfunction.py
+++ Extractfunction.py
@@ -1,19 +1,21 @@
 import os
 import sys
 import subprocess
-def CtagsParser(argv):
+
+def Parser(argv):
 	with open('tags') as f:
 		regf = open("regs.txt",'w')
 		String=''
 		for line in f:
-			String += line.split()[3] +" " + line.split()[2]+" "+line.split()[0]+"\n"
+			splitLine = line.split()
+			String += splitLine[3] +" " + splitLine[2]+" "+splitLine[0]+"\n"
 		regf.write(String)
 		regf.close()
 
 def ParsingFunction(argv):
 	DiffString = ["diff","--changed-group-format='%>'","--unchanged-group-format=''"]
 	String = ''
-	directories =  os.walk(sys.argv[1])
+	directories =  os.walk(argv[1])
 	split = argv[2].split('/')[0]
 	for (path,dir,files) in directories:
 		for filename in files:
@@ -23,30 +25,43 @@ def ParsingFunction(argv):
 
 			if (filename.split(".")[-1] != "cpp" or os.path.exists(Path1) != True ):
 				continue
+
 			DiffString.append(Path1)
 			DiffString.append(Path2)
 			DiffString.append("--new-line-format="+Path2+"' %dn %L'")
+
 			Pipe = subprocess.Popen(DiffString,stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 			DiffString = DiffString[:-3]
+#			print("aa")
 			String += Pipe.stdout.read().replace('\'','')
+
+	if ( String == ''):
+		print("No difference discovered")
 
 	String = String.split('\n')
 	f = open('regs.txt')
 	diff = float('inf')
+
 	answer = []
 	FunctionData = f.read().split('\n')
-	for i in range(100):
+
+	for i in range(len(String)-1):
 		FileName2 = String[i].split()[0]
 		DiffLine  = String[i].split()[1]
+		answer.append('x')
 		for j in range(len(FunctionData)-1):
 			FileName1 = FunctionData[j].split()[0]
 			FunctionName = FunctionData[j].split()[2]
 			FunctionLine = FunctionData[j].split()[1]
+
 			if (FileName1 == FileName2):
 				temp = int(DiffLine) - int(FunctionLine)
+
 				if(temp > 0 and temp < diff):
 					diff = temp
+					answer = answer[:-1]
 					answer.append(FunctionName+"\n")
+
 		diff = float('inf')
 	answer = list(set(answer))
 	AnswerFile = open("answer.txt","w")
@@ -55,7 +70,7 @@ def ParsingFunction(argv):
 	f.close()
 
 def main(argv):
-	CtagsParser(argv)
+	Parser(argv)
 	ParsingFunction(argv)
 
 if __name__ == "__main__":
