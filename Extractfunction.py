1eef6dd0db3d5caa85d55601bae0511916a32d69
signal <signal@kaist.ac.kr>
2017-06-05 03:04:44 +0900

    Modify Extractfunction

--- Extractfunction.py
+++ Extractfunction.py
@@ -2,82 +2,103 @@ import os
 import sys
 import subprocess
 
-def Parser(argv):
 
-	tagfile = argv[1][:-1] +"_function_tags"
-	execute="ctags -R -x --c++-kinds=+p --fields=iaSnt --languages=c++ --exclude=*.h "+argv[1]+"| grep function > "+tagfile
-	os.system(execute)
-	print(execute)
-	print(tagfile)
-	with open(tagfile) as f:
-		regf = open("regs.txt",'w')
-		String=''
-		for line in f:
-			splitLine = line.split()
-			String += splitLine[3] +" " + splitLine[2]+" "+splitLine[0]+"\n"
-		regf.write(String)
-		regf.close()
+def function_parser(argv):
+    print ("Parsing fuctions..")
+    tagName = os.path.basename(argv[1])
+    tagFileName = tagName + "_function_tags"
 
-def ParsingFunction(argv):
-	DiffString = ["diff","--changed-group-format='%>'","--unchanged-group-format=''"]
-	String = ''
-	directories =  os.walk(argv[1])
-	split = argv[2].split('/')[0]
-	for (path,dir,files) in directories:
-		for filename in files:
-			Path1 = path
-			Path1 = split +'/'+ '/'.join(Path1.split('/')[1:]) +'/'+filename
-			Path2 = os.path.join(path,filename)
+    if (os.path.exists(tagFileName) != True):
+        ctagCommand ="ctags -R -x --c++-kinds=+p --fields=iaSnt --languages=c++ \
+                    --exclude=*.h "+ argv[1] +"| grep function > "+ tagFileName
+        os.system(ctagCommand)
 
-			if (filename.split(".")[-1] != "cpp" or os.path.exists(Path1) != True ):
-				continue
+    tagFile = open(tagFileName)
+    functionString=[]
+    for line in tagFile:
+        lineSplit = line.split()
+        functionString.append(lineSplit[3] + " " + lineSplit[2] + " " + lineSplit[0])
+    print ("Parsing Done")
+    return functionString
 
-			DiffString.append(Path1)
-			DiffString.append(Path2)
-			DiffString.append("--new-line-format="+Path2+"' %dn %L'")
+def diff_file(argv):
+    print ("Diff strings..")
+    diffCommand = ["diff", "--changed-group-format='%>'", "--unchanged-group-format=''"]
+    diffString = []
+    directories = os.walk(argv[1])
+    split = argv[2].split('/')[0]
 
-			Pipe = subprocess.Popen(DiffString,stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-			DiffString = DiffString[:-3]
-#			print("aa")
-			String += Pipe.stdout.read().replace('\'','')
+    for (path, _, files) in directories:
+        for filename in files:
+            Path1 = path
+            Path1 = split +'/'+ '/'.join(Path1.split('/')[1:]) +'/'+filename
+            Path2 = os.path.join(path,filename)
 
-	if ( String == ''):
-		print("No difference discovered")
+            if (filename.split(".")[-1] != "cpp" or os.path.exists(Path1) != True ):
+                continue
+            diffCommand.append("--new-line-format="+Path2+"' %dn %L'")
+            diffCommand.append(Path1)
+            diffCommand.append(Path2)
 
-	String = String.split('\n')
-	f = open('regs.txt')
-	diff = float('inf')
+            pipe = subprocess.Popen(diffCommand,stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            diffCommand = diffCommand[:-3]
+            diffResult = pipe.stdout.read().replace('\'','')
+            if diffResult != '':
+                diffString.append(diffResult)
 
-	answer = []
-	FunctionData = f.read().split('\n')
+    if ( diffString == []):
+        print("No difference discovered")
+        sys.exit(0)
 
-	for i in range(len(String)-1):
-		FileName2 = String[i].split()[0]
-		DiffLine  = String[i].split()[1]
-		answer.append('x')
-		for j in range(len(FunctionData)-1):
-			FileName1 = FunctionData[j].split()[0]
-			FunctionName = FunctionData[j].split()[2]
-			FunctionLine = FunctionData[j].split()[1]
+    print("Diff Done")
+    return diffString
 
-			if (FileName1 == FileName2):
-				temp = int(DiffLine) - int(FunctionLine)
+def extract_modified_function(funcString, diffString):
 
-				if(temp > 0 and temp < diff):
-					diff = temp
-					answer = answer[:-1]
-					answer.append(FunctionName+"\t"+FileName1+"\n")
+    '''
+    Compare Diff line and Function line
+    '''
+    print ("Starting compare...")
+    answer = []
+    for i in range(len(diffString)-1):
+        minDiff = float('inf')
+        diffString_split = diffString[i].split()
+        fileName1 = diffString_split[0]
+        diffLine  = diffString_split[1]
 
-		diff = float('inf')
-	answer = list(set(answer))
-	AnswerFile = open("answer.txt","w")
-	AnswerFile.write(''.join(answer))
-	AnswerFile.close()
-	f.close()
+        for j in range(len(funcString)-1):
+            funcString_split = funcString[j].split()
+            fileName2 = funcString_split[0]            
+            functionLine = funcString_split[1]
+            functionName = funcString_split[2]
+
+            if (fileName1 == fileName2):
+                diffLineNum = int(diffLine) - int(functionLine)
+                if(diffLineNum > 0 and minDiff > diffLineNum):
+                    minDiff = diffLineNum
+                    diffFunc = functionName
+                    diffFile = fileName1
+
+        if (minDiff != float('inf')):
+            answer.append(diffFunc+"\t"+diffFile+"\n")
+
+    if answer == []:
+        print ("No function modified")
+        sys.exit(0)
+
+    answer = list(set(answer))
+    answerFile = open("answer.txt","w")
+    answerFile.write(''.join(answer))
+    answerFile.close()
+    print ("Check answer.txt")
 
 def main(argv):
-	Parser(argv)
-	ParsingFunction(argv)
+    if (len(argv) < 3):
+        print("Usage: python [fileName] [Unpatched directory] [patched directory]")
+        sys.exit(0)
+    fString = function_parser(argv)
+    diffString = diff_file(argv)
+    extract_modified_function(fString, diffString)
 
 if __name__ == "__main__":
-	main(sys.argv)
+    main(sys.argv)
